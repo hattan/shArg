e48f14da9ea7e8cd58c96490e43293898ad490fb
Hattan Shobokshi <hattan@gmail.com>
2021-01-25 23:04:46 -0800

    added hook example

---
+++ examples/hook.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# #########################################################
+# Navigate to the examples folder and invoke this file.
+# cd examples
+# ./simple_example.sh -m hello -d
+# #########################################################
+
+# load shArg
+source ../scripts/shArg.sh
+
+declare MESSAGE
+
+# Declare a hook method to fire when the message argument is found
+# Hook methods allow you to run a piece of code for additional behaviors when an argument is bound
+messageHook() {
+  local message=$1
+
+  echo "message hooked invoked with $message"
+  echo "Global variable is still available $MESSAGE"
+
+}
+
+# register arguments
+shArgs.arg "MESSAGE" -m --message PARAMETER true messageHook
+
+# parse inputs
+shArgs.parse $@
+
+
