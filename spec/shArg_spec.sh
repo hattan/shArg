fe5360988c07e30a129e0cf43c1fef99558e2432
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2021-03-07 14:18:09 -0800

    added support for csv input to array

--- spec/shArg_spec.sh
+++ spec/shArg_spec.sh
@@ -172,6 +172,41 @@ Describe 'shArg'
       When call shArgs.val "INVALID"
       The output should equal ""
     End
-
   End 
+
+  Context "list inputs"
+    setup() { 
+      shArgs.arg "IPS" -i --ips PARAMETER true 
+    }
+    BeforeEach 'setup'
+
+    assert_val_method() {
+      local varName=$1
+      local expectedValue=$2
+      local value=$(shArgs.val "IPS")
+      if [ "$value" == "$expectedValue" ]; then
+        return 0
+      else
+        return 1
+      fi
+    }
+
+    assert_global_ips() {
+      local expectedValue=$1
+      if [ "$IPS" == "$expectedValue" ]; then
+        return 0
+      else
+        return 1
+      fi
+    }
+    It 'should return an array of ips when shArg.val is called'
+      When call shArgs.parse -i 10.10.10.1,10.10.10.2
+      Assert assert_val_method "IPS" "10.10.10.1 10.10.10.2"
+    End
+
+    It 'should set the global variable IPS to an arra of ips'
+      When call shArgs.parse -i 10.10.10.1,10.10.10.2
+      Assert assert_global_ips  "10.10.10.1 10.10.10.2"
+    End
+  End   
 End
